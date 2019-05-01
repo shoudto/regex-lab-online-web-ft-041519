0ef14db61858cdf04a651e2c35908a9f6e300430
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 20:47:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   !!text.capitalize.scan(/\A+UN\z+ing/)
+   !!text.capitalize.scan(/\A+Un\z+ing/)
 end
 
 def words_five_letters_long(text)
