48254b4ef43b2b2e7cc707a9d41f4373034e2c3d
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 20:38:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,11 +2,7 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
   #binding.pry
-  if word.match(/\w [aeiou]/) == true 
-    return true 
-  else 
-    return false 
-  end 
+   !!word.capitalize.match(/\A+[AEIOU]/) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
